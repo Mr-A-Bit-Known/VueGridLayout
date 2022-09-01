79463f163054313876142127c0e527f1ea643ad9
unknown <1501996728@qq.com>
2022-09-01 15:14:51 +0800

    初步路由设置完成

---
+++ vue-grid-layout/src/Pages/Register.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="registerWrapper">注册界面</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.registerWrapper {
+  font-size: 20px;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+}
+</style>
--- vue-grid-layout/src/router/index.js
+++ vue-grid-layout/src/router/index.js
@@ -1,15 +1,36 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '../Pages/HelloWorld.vue'
+import Register from '../Pages/Register.vue'
+import PagesNotFound from '../Pages/PagesNotFound.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: "history",
   routes: [
     {
+      // 默认路由重定向
       path: '/',
-      name: 'HelloWorld',
+      redirect: "/Pages/HelloWorld"
+    },
+    // 默认HelloWorld为登陆界面
+    {
+      path: "/Pages/HelloWorld",
+      naem: "HelloWorld",
       component: HelloWorld
+    },
+    // 注册界面
+    {
+      path: '/Pages/register',
+      name: 'Register',
+      component: Register
+    },
+    // 404
+    {
+      path: "/Pages/404NotFound",
+      name: "PagesNotFound",
+      component: PagesNotFound
     }
   ]
 })
